2575b42212a2f5e7e92a44fb675ede360263a5eb
magusafr <40602197+magusafr@users.noreply.github.com>
2022-10-02 00:20:22 +0700

    Create category_name.sql

---
+++ category_name.sql
@@ -0,0 +1,14 @@
+-- lower case
+UPDATE iowa_drink_sales SET category_name = LOWER(category_number);
+
+-- Inconsistent category name
+UPDATE iowa_drink_sales SET category_name = 'american cordials & liqueurs' WHERE category_name = 'american cordials & liqueur';
+UPDATE iowa_drink_sales SET category_name = 'american distilled spirits specialty' WHERE category_name = 'new star pella';
+UPDATE iowa_drink_sales SET category_name = 'american vodkas' WHERE category_name = 'american distilled spirit specialty';
+UPDATE iowa_drink_sales SET category_name = 'cocktails / rtd' WHERE category_name = 'cocktails /rtd';
+UPDATE iowa_drink_sales SET category_name = 'imported cordials & liqueurs' WHERE category_name = 'imported cordials & liqueur';
+UPDATE iowa_drink_sales SET category_name = 'imported distilled spirits specialty' WHERE category_name = 'imported distilled spirit specialty';
+UPDATE iowa_drink_sales SET category_name = 'imported vodkas' WHERE category_name = 'imported vodka';
+UPDATE iowa_drink_sales SET category_name = 'iowa distillery whiskies' WHERE category_name = 'iowa distilleries';
+UPDATE iowa_drink_sales SET category_name = 'temporary & specialty packages' WHERE category_name = 'temporary  & specialty packages';
+UPDATE iowa_drink_sales SET category_name = 'temporary & specialty packages' WHERE category_name = 'temporary &  specialty packages';
