f55ff527d6c1505eda866ccfe2d1546cb745cf19
magusafr <40602197+magusafr@users.noreply.github.com>
2022-10-02 01:05:24 +0700

    Update delete_columns.sql

--- delete_columns.sql
+++ delete_columns.sql
@@ -2,6 +2,7 @@
 -- we will use city, and county data from https://data.opendatasoft.com/explore/dataset/georef-united-states-of-america-zc-point%40public/table/
 -- the data above will be joined with iowa data using zip_code as identifier
 -- we use city and county data from source above because that data are complete, while city and county data in iowa dataset are incomplete
+-- delete columns source: https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-drop-column/
 
 ALTER TABLE iowa_drink_sales 
   DROP COLUMN address,
