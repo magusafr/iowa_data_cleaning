45fcde68443fa3351658a092c39fe9ce093febcc
magusafr <40602197+magusafr@users.noreply.github.com>
2022-10-02 00:47:06 +0700

    Create item_description.sql

---
+++ item_description.sql
@@ -0,0 +1,195 @@
+-- lower case
+UPDATE iowa_drink_sales SET item_description = LOWER(item_description);
+
+-- Inconsistent category name
+UPDATE iowa_drink_sales SET item_description = 'absolut swedish vodka 80prf' WHERE item_description = 'absolut swedish vodka 80 prf';
+UPDATE iowa_drink_sales SET item_description = 'absolut swedish vodka 80prf mini' WHERE item_description = 'absolut swedish vodka 80 prf mini';
+UPDATE iowa_drink_sales SET item_description = 'ancnoc blas' WHERE item_description = 'ancnoc blas scotch';
+UPDATE iowa_drink_sales SET item_description = 'appleton 8yr reserve' WHERE item_description = 'appleton 8 year old reserve';
+UPDATE iowa_drink_sales SET item_description = 'appleton estate 21yr' WHERE item_description = 'appleton estate 21 yr jamaica rum'';
+UPDATE iowa_drink_sales SET item_description = 'appleton rare blend 12yr' WHERE item_description = 'appleton rare blend';
+UPDATE iowa_drink_sales SET item_description = 'appleton rare blend 12yr' WHERE item_description = 'appleton rare blend 12yr - use code 943803';
+UPDATE iowa_drink_sales SET item_description = 'ardbeg an oa smoker vap' WHERE item_description = 'ardbeg an oa 6 750ml smoker vap';
+UPDATE iowa_drink_sales SET item_description = 'arrow apricot brandy' WHERE item_description = 'arrow apricot flav brandy';
+UPDATE iowa_drink_sales SET item_description = 'arrow black raspberry liqueur' WHERE item_description = 'arrow blackraspberry liqueur';
+UPDATE iowa_drink_sales SET item_description = 'arrow blackberry brandy' WHERE item_description = 'arrow blackberry flav brandy';
+UPDATE iowa_drink_sales SET item_description = 'arrow wild cherry brandy' WHERE item_description = 'arrow wild cherry flav brandy';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+UPDATE iowa_drink_sales SET item_description = '' WHERE item_description = '';
+
+
+
+
+
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'�','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'°','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'''','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'/',' ');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'"','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'"','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'.','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'-','');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'    ',' ');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'   ',' ');
+UPDATE iowa_drink_sales SET item_description = REPLACE(item_description,'  ',' ');
