3f6e3bc3a0994d9d9384a8b5b64e0568c8c25871
magusafr <40602197+magusafr@users.noreply.github.com>
2022-10-02 16:08:25 +0700

    Create item_category_duplicates.sql

---
+++ item_category_duplicates.sql
@@ -0,0 +1,17 @@
+-- check category duplicates for each store_name
+SELECT DISTINCT(item_description, category_name)
+FROM iowa_drink_sales
+WHERE store_name IN
+                  (
+                  SELECT item_description
+                  FROM  
+                         iowa_drink_sales
+                  GROUP BY 
+                         item_description
+                  HAVING 
+                         COUNT(DISTINCT(category_name)) > 1
+                  ORDER BY 
+                         item_description
+                  ) 
+  
+ORDER BY 1;
